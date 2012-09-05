720de830b2e9ae7a7c26921df7ea9b286cb12ff9
Sean Schwartz <unvcustoms@gmail.com>
2012-09-05 13:03:53 -0500

    I changed things.

--- AntiDupe/src/net/justicecraft/AntiDupe/Core.java
+++ AntiDupe/src/net/justicecraft/AntiDupe/Core.java
@@ -1,5 +1,7 @@
 package net.justicecraft.AntiDupe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -8,6 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Core extends JavaPlugin{
 	public final Logger log = Logger.getLogger("Minecraft");
 	public static String logPrefix = "[AntiDupe] ";
+	public static List<String> recentDeaths = new ArrayList<String>();
 	
 	// On Disable
 	public void onDisable(){
@@ -19,5 +22,12 @@ public class Core extends JavaPlugin{
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.log.info(pdfFile.getName()+ " version " + pdfFile.getVersion() + " is enabled.");
 		getServer().getPluginManager().registerEvents(new PListener(this), this);
+		getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
+			public void run(){
+				if(!recentDeaths.isEmpty()){
+					recentDeaths.clear();
+				}
+			}
+		}, 20L, 20L);
 	}
 }
--- AntiDupe/src/net/justicecraft/AntiDupe/PListener.java
+++ AntiDupe/src/net/justicecraft/AntiDupe/PListener.java
@@ -1,9 +1,17 @@
 package net.justicecraft.AntiDupe;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World.Environment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.PlayerInventory;
 
 
 public class PListener implements Listener{
@@ -13,9 +21,37 @@ public class PListener implements Listener{
 	}
 	
 	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event){
+		if(event.getEntity() instanceof Player){
+			Player p = (Player)event.getEntity();
+			if(p == null){
+				event.getDrops().clear();
+			} else {
+				if(!plugin.recentDeaths.contains(p.getDisplayName())){
+					plugin.recentDeaths.add(p.getDisplayName());
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event){
+		if(plugin.recentDeaths.contains(event.getPlayer().getDisplayName())){
+			PlayerInventory pinv = event.getPlayer().getInventory();
+			pinv.clear();
+		}
+	}
+	
+	@EventHandler
 	public void onInventoryClick(InventoryClickEvent event){
 		if(event.isCancelled()){
 			return;
+		}
+		if(event.getInventory().getType() == InventoryType.BREWING){
+			if(event.isShiftClick()){
+				event.setCancelled(true);
+				return;
+			}
 		}
 		if(event.getCursor().getType() == Material.AIR){
 			return;
@@ -30,9 +66,21 @@ public class PListener implements Listener{
 		}
 		if(event.isLeftClick() && event.getCursor().getAmount() <= 0){
 			event.setCancelled(true);
-//			event.getCursor().setType(Material.AIR);
 			plugin.log.info(plugin.logPrefix + event.getWhoClicked().getName()+" attempted to dupe "+event.getCursor().getType());
 			return;
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerTeleport(PlayerTeleportEvent event){
+		if(event.isCancelled()){
+			return;
+		}
+		Location loc = event.getPlayer().getLocation().subtract(0, 1, 0);
+		if(loc.getWorld().getEnvironment() == Environment.THE_END){
+			if(plugin.getServer().getWorld(loc.getWorld().getName()).getBlockAt(loc) != null  && plugin.getServer().getWorld(loc.getWorld().getName()).getBlockAt(loc).getType() == Material.AIR){
+				event.setCancelled(true);
+			}
 		}
 	}
 }
